00c1c55115b8df1136f1416b6077738e667527b7
Shamil Kh <daglex@users.noreply.github.com>
2023-06-16 02:41:57 +0300

    Add files via upload

---
+++ theme-info.php
@@ -0,0 +1,21 @@
+<?php return array (
+
+  'index' => 0,
+
+  'display_name' => array (
+    'en' => 'Birman',
+    'ru' => 'Бирман',
+  ),
+
+  'colors' => array (
+    'background' => '#fff',
+    'headings' => '#000',
+    'text' => '#000',
+    'link' => '#70a0b0',
+  ),
+  
+  'based_on' => 'plain',
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+  'supports_dark_mode' => true,
+
+); ?>
